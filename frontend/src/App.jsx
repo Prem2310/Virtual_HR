0a5afbbfc51658ca129050fbc9c0d119a780c668
KUMAIL261103 <kumailghadi2611@gmail.com>
2024-04-21 04:32:32 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Home from "./pages/home";
 import Hero from "./pages/Hero";
-
+import { Experience } from "./components/Experience";
 function App() {
   return (
     <div className="App">
@@ -14,6 +14,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/experience" element={<Experience />} />
         </Routes>
       </Router>
     </div>
